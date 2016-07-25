d18d1ffa6f57cab1b606377a3ed974259c3ec645
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2016-07-25 21:52:00 +0530

    Checking Enable Bluetooth

--- src/com/example/Trends/MyActivity.java
+++ src/com/example/Trends/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.Trends;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,24 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onBluetooth(View view){
+        setBluetooth(true);
+    }
+
+    private boolean setBluetooth(boolean enable){
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        boolean isEnabled = bluetoothAdapter.isEnabled();
+
+        if(enable && !isEnabled){
+            return  bluetoothAdapter.enable();
+        }
+        else if (!enable && isEnabled){
+            return bluetoothAdapter.disable();
+        }
+        //No need to change bluetooth state.
+        return true;
+
     }
 }
